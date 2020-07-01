9abc864ed3319dc76216d8203d68d3cad2b9a8d0
475Fatema <66346391+475Fatema@users.noreply.github.com>
2020-07-01 22:09:23 +0300

    تم

---
+++ Development/js/scripts.js
@@ -0,0 +1,89 @@
+let menu = parseInt(prompt("select your food number"));
+
+switch(menu) {
+    case 1:
+      console.log ("burger");
+      break;
+    case 2:
+      console.log(" pizza");
+      break;
+    case 3:
+      console.log("sushi");
+      break;
+    default :
+      console.log("your dish is not avalible");
+}
+
+let number1 = parseInt(prompt("ادخل الرقم الاول"));
+let number2 = parseInt(prompt(" ادخل لرقم الثاني"));
+let operation = prompt(" (+,-,*,/)ادخل العملية الحسابية  ");
+if (number1 && number2); {
+    { 
+if (operation=="*,+,-"); {
+    
+ console.log (number1*number2); }
+
+    } 
+    
+    if (operation=="/"){
+
+    parseInt(alert("  العملية غير صحيحة  "));
+
+    }
+
+}
+
+// let age = 2020 - birthyear;
+let birthyear = prompt("enter your birthyear ");
+// جامعة 2002
+if ("age>=18") {
+  alert("جامعة");
+  
+}
+else
+// مدرسة 2004
+if ("age==16") {
+  alert("مدرسة");
+}
+// روضة 2016
+else
+if ("age=4") {
+  alert("روضة");
+  
+}
+
+
+
+// switch( "school year") {
+//   case ( age == 18):
+    // let age = 2020 - birthyear;
+//     alert (" انت في الثانوي" );
+//     break;
+//   case ( age ==4):
+//     let age = 2006 - birthyear
+//     alert(" انت في الروضة")
+//     break;
+//   default:
+//     // code block
+// }
+
+
+
+// const birthyear = prompt("enter your birthyear ")
+// let age = 2020 - birthyear;
+//  alert (" انت في الثانوي" );
+
+//  let age = 2006 - birthyear
+//  alert(" انت في الروضة")
+  
+    
+
+
+
+
+
+
+
+
+
+
